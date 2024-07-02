ec134769d5729bf8aa5429e69f6b8b26d768738b
Kinza Sheikh <75093626+KinzaaSheikh@users.noreply.github.com>
2024-07-02 23:32:16 +0500

    Configured tailwind

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { configureStore } from "@reduxjs/toolkit"
-import todoSlice from '../features/todo/todoSlice'
+import todoReducer from '../features/todo/todoSlice'
 
 export const store = configureStore({
-    reducer: todoSlice,
+    reducer: todoReducer,
 })
